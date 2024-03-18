e24906cf437ad76aa8549bc2f5d738986b262358
RonK-0 <ronkua12@gmail.com>
2024-03-18 13:53:10 +0800

    copycat home

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,16 @@ module.exports = {
       colors:{
         dark: "#2b2b2b",
         light: "#fff",
-        accent: "#006341"
+        accent: "#006341",
+        accent_alt: "#1e3932",
+        c1: "#d5d0ea",
+        c2: "#f2f0eb",
+        c3: "#cee8c9",
+        c4: "#f5cac4",
+        c5: "#dbe6f2",
+        c6: "#d5d0ea",
+        c7: "#e99264",
+        c8: "#006241"
       }
     },
   },
